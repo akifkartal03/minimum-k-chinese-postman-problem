c82d413d976422f7c8bb699e2113954daf92d41d
akifkartal03 <akokartal@gmail.com>
2022-04-05 22:34:44 +0300

    start to project

---
+++ code/Graph.py
@@ -0,0 +1,19 @@
+
+#Abstract representation of a graph.
+class Graph:
+
+
+    def __init__(self, ):
+        self.edges = {}
+
+
+    def __repr__(self):
+        print("graff.")
+
+    def add_edges(self, edges):
+        print("add new edges")
+
+
+    def remove_edges(self, edges):
+        print("remove edges")
+
