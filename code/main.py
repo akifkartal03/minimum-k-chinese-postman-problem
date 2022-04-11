69dfddcd6474909afcded705785ecee96ef69072
akifkartal03 <akokartal@gmail.com>
2022-04-12 00:09:19 +0300

    start to algorithm

--- code/main.py
+++ code/main.py
@@ -1,1 +1,40 @@
-print("algorithm_file")
+from my_graph import *
+
+
+# I didn't want to bother with global variables therefore,
+# I created a class to encapsulate my algorithm.
+class MyAlgorithm:
+
+    def __init__(self, my_graph):
+        self.__my_graph = my_graph
+        self.__sorted_edges = None
+        self._closed_walks = None
+
+    def my_algorithm(self, s, k):
+        print("algo")
+
+    def sort_edges_descending(self):
+        weights = self.__my_graph.get_weights()
+        self.__sorted_edges = self.__my_graph.get_edges()
+        print(weights)
+        print(self.__sorted_edges)
+        n = len(weights)
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if weights[j] < weights[j + 1]:
+                    self.__sorted_edges[j], self.__sorted_edges[j + 1] = \
+                        self.__sorted_edges[j + 1], self.__sorted_edges[j]
+                    weights[j], weights[j + 1] = weights[j + 1], weights[j]
+        print("sorted:")
+        print(self.__sorted_edges)
+
+    def create_closed_walk(self):
+        print("sorted:")
+
+
+
+graph = MyGraph()
+graph.generate_random_graph(5, 10)
+graph.print_graph(2)
+alg = MyAlgorithm(graph)
+alg.sort_edges_descending()
--- code/my_graph.py
+++ code/my_graph.py
@@ -12,7 +12,7 @@ class MyGraph:
     def generate_random_graph(self, number_of_vertex, number_of_edges):
         self.__g = Graph()
         self.__g.add_vertices(number_of_vertex)
-        # Add ids and labels to vertices
+
         for i in range(len(self.__g.vs)):
             self.__g.vs[i]["id"] = i
             self.__g.vs[i]["label"] = i
@@ -22,56 +22,44 @@ class MyGraph:
             value = random.sample(range(0, self.__g.vcount()), 2)
             if value not in rand_edges:
                 rand_edges.append(value)
-        print(rand_edges)
         self.__g.add_edges(rand_edges)
         rand_weights = []
         for x in range(0, len(rand_edges)):
             rand_weights.append(random.randint(5, 40))
-        print(rand_weights)
-        print(len(rand_edges))
-        print(len(rand_weights))
         self.__g.simplify(multiple=True, loops=True, combine_edges=None)
         self.__g.es['weight'] = rand_weights
         self.__g.es['label'] = rand_weights
         self.__g.es["curved"] = False
-
-        print("-------summary-----")
-        summary(self.__g)
-        print("-----edge list------")
-        print()
-
-        # plot(self.__g)
 
     def print_graph(self, graph_number):
         visual_style = {}
 
         out_name = "outputs/graph" + str(graph_number) + ".png"
 
-        # Set bbox and margin
         visual_style["bbox"] = (400, 400)
         visual_style["margin"] = 27
 
-        # Set vertex colours
         visual_style["vertex_color"] = 'white'
 
-        # Set vertex size
         visual_style["vertex_size"] = 45
 
-        # Set vertex lable size
         visual_style["vertex_label_size"] = 22
 
-        # Don't curve the edges
         visual_style["edge_curved"] = False
 
-        # Set the layout
         my_layout = self.__g.layout_lgl()
         visual_style["layout"] = my_layout
 
-        # Plot the graph
         plot(self.__g, out_name, **visual_style)
 
     def get_edges(self):
         return self.__g.get_edgelist()
+
+    def get_graph(self):
+        return self.__g
+
+    def get_weights(self):
+        return self.__g.es['weight']
 
     def print_info(self):
         print("----------Information-----------")
@@ -92,17 +80,25 @@ class MyGraph:
         if len(path[0]) > 0:
             # Add up the weights across all edges on the shortest path
             distance = 0
-            for e in path[0]:
-                distance += self.__g.es[e]["weight"]
+            n = len(path[0])
+            for i in range(n):
+                if i + 1 != n:
+                    mytuple = (path[0][i],path[0][i+1])
+                    distance += self.get_weight_by_index(self.get_edges().index(mytuple))
             return distance
         else:
-            print("End node could not be reached!")
+            print("shortest path is empty")
             return 0
 
+    def get_weight_by_index(self, index):
+        return (self.__g.es["weight"])[index]
 
+"""
 graph = MyGraph()
 graph.generate_random_graph(5, 10)
-graph.print_graph(1)
-graph.print_info()
-print(graph.get_shortest_path(0,4))
+graph.print_graph(2)
+path=graph.get_shortest_path(0,4)
+print(path[0])
 print(graph.get_edges())
+print(graph.get_shortest_path_length(path))
+"""
